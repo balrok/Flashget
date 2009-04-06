5ac1b58f03b0858cc81ace8b9e5ea21fff2ac9fb
balrok <der-coole-carl@gmx.net>
2009-04-06 10:40:56 +0000

    colored info and error-output

--- tools/logging.py
+++ tools/logging.py
@@ -1,7 +1,19 @@
+
+def color(color, str):
+# http://www.siafoo.net/snippet/88
+    if color is 'red':
+        str = '\033[1;31m' + str
+    if color is 'yellow':
+        str = '\033[1;33m' + str
+    return str+'\033[1;m'
+
+
 class LogHandler(object):
     def __init__(self, type):
         self.type = type
     def info(self, str):
+        str = '[' + color('yellow', self.type) + ']: '+ str
         print str
     def error(self, str):
+        str = '[' + color('red', self.type) + ']: '+ str
         print str
