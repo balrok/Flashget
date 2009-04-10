03d47a0011dc7841bffc2856d49ab3d43ba157f7
balrok <der-coole-carl@gmx.net>
2009-04-10 23:15:30 +0000

    check if cache/flash-dir is writable at configload

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
+import os
 
 class config(object):
     cache_dir = '/mnt/sda6/prog/flashget/cache'
     flash_dir = '/mnt/sda6/prog/flashget/flash'
-    # TODO: os.access(path, os.W_OK)
+
+
+
+
+
+    if not os.access(cache_dir, os.W_OK):
+        print "your cache-dir isn't writeable please edit config.py"
+    if not os.access(flash_dir, os.W_OK):
+        print "your flash-dir isn't writeable please edit config.py"
