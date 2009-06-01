e00be4994d2e0e3a27a344d7d02bc811ac2b8412
balrok <der-coole-carl@gmx.net>
2009-06-01 01:24:28 +0000

    added documentation + a new parameter to helper.textextract
    the new parameter now allows also an easy textextractall

--- tools/helper.py
+++ tools/helper.py
@@ -24,20 +24,27 @@ def normalize_title(str):
     return str.replace('/', '_')
 
 
-def textextract(data, startstr, endstr):
+def textextract(data, startstr, endstr, startpos = 0):
+    ''' extracts a text from data, which is between startstr and endstr
+        if startstr is '' it will extract from the beginning of data
+        if endstr   is '' it will extract until the end of data
+        the optional parameter startpos will indicate the startposition from where startstr will be searched
+        and if startpos is something else than 0 it will return a tuple of the extracted string and the endposition of this string '''
     if startstr == '':
-        pos1 = 0
+        pos1 = startpos
     else:
-        pos1=data.find(startstr)
+        pos1 = data.find(startstr, startpos)
         if pos1 < 0:
-            return
+            return None
         pos1 += len(startstr)
 
     if endstr == '':
         return data[pos1:]
     pos2 = data.find(endstr, pos1)
     if pos2 < 0:
-        return
+        return None
+    if startpos != 0:
+        return (data[pos1:pos2], pos2)
     return data[pos1:pos2]
 
 
@@ -54,7 +61,6 @@ def textextractall(data, startstr, endstr):
             return foundlist
         startpos = pos2 + len(endstr) + 1
         foundlist.append(data[pos1:pos2])
-
 
 class SmallId(object):
     ''' this class is used to produce small ids
