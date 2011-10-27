303b9698de923e25605faa0fada4663b7c8a58ec
carl <carl.schoenbach@googlemail.com>
2011-10-27 11:48:21 +0200

    removed imeem

--- tools/defines.py
+++ tools/defines.py
@@ -14,7 +14,6 @@ class Stream:
     HDWEB      = 4
     SEVENLOAD  = 5
     YOUTUBE    = 6
-    IMEEM      = 7
     HDSHARE    = 8
     PLAIN      = 9
     ZEEC       = 10
@@ -22,8 +21,8 @@ class Stream:
     CCF        = 12
     DLC        = 13
     VIDEOBB    = 14
-    #      0     1      2             3         4      5            6           7       8           9      10      11     12     13     14
-    str = ('', 'Veoh', 'EatLime', 'MegaVideo', 'HDWeb', '7Load', 'YouTube', 'Imeem', 'HDShare', 'Plain', 'Zeec', 'xvid', 'CCF', 'DLC', 'VideoBB')
+    #      0     1      2             3         4      5            6        7       8           9      10      11     12     13     14
+    str = ('', 'Veoh', 'EatLime', 'MegaVideo', 'HDWeb', '7Load', 'YouTube', '', 'HDShare', 'Plain', 'Zeec', 'xvid', 'CCF', 'DLC', 'VideoBB')
 
 class Quality:
     LOW  = 0
