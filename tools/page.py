22f672291ca06141200d5397f0c581b1acbee305
carl <carl.schoenbach@googlemail.com>
2011-10-28 22:28:05 +0200

    predocumentation for upcomming page changes

--- tools/page.py
+++ tools/page.py
@@ -3,6 +3,26 @@ import tools.defines as defs
 
 log = config.logger['page']
 
+
+# TODO:
+# A Page describes a website where a movie or serie is located
+# this movie/serie contains a name and 1-* parts
+# Each part can contain 1-* alternative Downloadlocations and a name and number
+# Each alternative Downloadlocation can contain 1-* parts where each gets a number and a url
+
+# Classes:
+#   * Page
+#       extracting the information, navigating, creates following classes
+# all following classes referencing each other top and down.. so Media<->Part<->Alternative<->AlternativePart
+#   * Media (Movie/Serie)
+#       contains information (most important the name)
+#   * Part
+#       contains number and name (for example a serie contains many parts which are numbered)
+#   * Alternative
+#       cause one part can be downloaded from multiple hosts or with different codecs/audio...
+#       contains additional description (codec, language)
+#   * AlternativePart
+#       contains the part-number and dl-url
 class Page(object):
     TYPE_UNK    = 0
     TYPE_MULTI  = 1
