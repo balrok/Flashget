4befee395b96dbaa3ed248ea8477ad21381c9887
carl <carl.schoenbach@googlemail.com>
2011-11-05 18:50:57 +0100

    hack for animeloads typo

--- tools/pages/animeloads.py
+++ tools/pages/animeloads.py
@@ -103,7 +103,10 @@ class AnimeLoads(Page):
                                     redirectUrl = UrlMgr({'url': redirectUrl.group(1), 'log': self.log, 'cookies': self.cookies})
                                     realUrl = re.search("http-equiv=\"refresh\" content=\".;URL=(.*?)\"", redirectUrl.data)
                                     if realUrl:
-                                        alternativePart.url = realUrl.group(1)
+                                        realUrl = realUrl.group(1)
+                                        if realUrl.startswith('ttp'):
+                                            realUrl = 'h'+realUrl
+                                        alternativePart.url = realUrl
                             if streamCurCol == 2:
                                 alternative.audio = re.findall("lang/(..)\.png", etree.tostring(streamColumn))
                             if streamCurCol == 3:
