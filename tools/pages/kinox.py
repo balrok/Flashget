c9084dfc02fea651390c1e492717ce102b80673a
carl <carl.schoenbach@googlemail.com>
2011-11-06 11:44:37 +0100

    kinox extracting works

--- tools/pages/kinox.py
+++ tools/pages/kinox.py
@@ -5,6 +5,7 @@ from lxml import html
 from lxml import etree
 import re
 import sys
+import json
 
 class Kinox(Page):
     def __init__(self):
@@ -16,8 +17,18 @@ class Kinox(Page):
         allPages = []
         return allPages
 
+    def checkPage(self, url, part):
+        if not url.data.find(part) > 0:
+            url.clear_connection()
+            url.setCacheWriteOnly()
+            if not url.data.find(part) > 0:
+                self.log.error('download problem?')
+                self.log.error(url.url)
+                self.log.error(url.data)
+        return url
+
     def extract(self, link):
-        url = UrlMgr({'url': link, 'log': self.log})
+        url = self.checkPage(UrlMgr({'url': link, 'log': self.log}), 'Stream online')
         origName = textextract(url.data, '<title>', ' Stream online anschauen und downloaden auf Kino</title>')
         name = origName
         if not origName:
@@ -33,13 +44,23 @@ class Kinox(Page):
             media = Page.getMedia(self, name, link)
             episodes = textextract(seasonSelect, 'value="'+season+'" rel="', '"').split(',')
             for episode in episodes:
-                part = media.getSub()
+                part = media.createSub()
                 part.name = episode
                 part.num = episode
-                log.error(getUrl+'&Season='+season+'&Episode='+episode)
-                url = UrlMgr({'url':getUrl+'&Season='+season+'&Episode='+episode, 'log':self.log})
+                url = self.checkPage(UrlMgr({'url':getUrl+'&Season='+season+'&Episode='+episode, 'log':self.log}), 'HosterList')
                 streams = textextractall(url.data, 'rel="', '"')
-                #for stream in streams:
+                for stream in streams:
+                    url = self.checkPage(UrlMgr({'url':'http://kinox.to/aGET/Mirror/'+stream.replace('amp;', ''), 'log':self.log}), 'HosterName')
+                    try:
+                        data = json.loads(url.data)
+                    except:
+                        log.error("no json")
+                    hoster = data['HosterName']
+                    hosterHome = data['HosterHome']
+                    streamLink = textextract(data['Stream'], 'href="', '"')
+                    altPart = part.createSub().createSub()
+                    altPart.url = streamLink
+                    self.setPinfo(altPart)
         return media
 
 urlPart = 'kinox.to' # this part will be matched in __init__ to create following class
