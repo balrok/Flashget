6e89c4a6afc082055a1c404cc6758666571d3f2c
carl <carl.schoenbach@googlemail.com>
2011-11-16 21:22:57 +0100

    wrap exceptionhandling around server

--- server.py
+++ server.py
@@ -9,6 +9,9 @@ import config
 import pickle
 
 
+class MyException(Exception):
+    pass
+
 caches = {}
 
 host = 'localhost'
@@ -24,7 +27,7 @@ while running:
 
     for s in inputready:
         flush += 1
-        if flush == 10:
+        if flush == 1:
             flush = 0
             sys.stdout.flush()
 
@@ -41,75 +44,56 @@ while running:
             # handle all other sockets
             try:
                 size = s.recv(8).rstrip()
-            except:
-                size = None
-            if not size:
-                s.close()
-                input.remove(s)
-                continue
-
-            size = int(size)
-            origSize = int(size)
-            data = ''
-            while size > 0:
-                try:
-                    chunk = s.recv(size)
-                except:
-                    chunk = ''
-                if chunk == '':
-                    print "ERROR chunk empty but not full size retrieved"
-                    break
-                data += chunk
-                size -= len(chunk)
-            else:
-                s.close()
-                input.remove(s)
-                continue
-
-            try:
-                data = pickle.loads(data)
-            except:
-                # this means data was not fully received
-                print origSize
-                print size
-                print len(data)
-                print data[:1000]
-                print "^error"
-                continue
-
-            command = data['c']
-            key = data['k']
-            directory = data['d']
-            value = data['v']
-            section = data['section']
-            #print (origSize, command, key, directory, value[:100])
-            if directory not in caches:
-                caches[directory] = Cache(directory)
-            cache = caches[directory]
-            cache.key = key
-            if command == 'lookup':
-                print "l",
-                #print "looking up: "+key
-                sendData = cache.lookup(section)
-                #if not sendData:
-                #    print "not found"
-                #else:
-                #    print "found"
-                sendData = pickle.dumps(sendData, 1)
-                size = str(len(sendData))
-                size += (8-len(size))*" "
-                try:
-                    s.sendall(size+sendData)
-                except:
+                if not size:
                     s.close()
                     input.remove(s)
                     continue
-            if command == 'write':
-                print "w",
-                #print "writing in: "+key+"/"+section+ ".. data: "+value[:100]
-                cache.write(section, value)
-            if command == 'remove':
-                print "r",
-                cache.remove(section)
+
+                size = int(size)
+                origSize = int(size)
+                data = ''
+                while size > 0:
+                    chunk = s.recv(size)
+                    if chunk == '':
+                        raise MyException("ERROR chunk empty but not full size retrieved")
+                    data += chunk
+                    size -= len(chunk)
+
+                data = pickle.loads(data)
+
+                command = data['c']
+                key = data['k']
+                directory = data['d']
+                value = data['v']
+                section = data['section']
+                if directory not in caches:
+                    caches[directory] = Cache(directory)
+                cache = caches[directory]
+                cache.key = key
+                if command == 'lookup':
+                    print "l",
+                    sendData = cache.lookup(section)
+                    sendData = pickle.dumps(sendData, 1)
+                    size = str(len(sendData))
+                    size += (8-len(size))*" "
+                    s.sendall(size+sendData)
+                if command == 'write':
+                    print "w",
+                    #print "writing in: "+key+"/"+section+ ".. data: "+value[:100]
+                    cache.write(section, value)
+                if command == 'remove':
+                    print "r",
+                    cache.remove(section)
+            except socket.error, (value,msg):
+                print "socket error"
+                print value,msg
+                s.close()
+                input.remove(s)
+            except MyException, msg:
+                print "custom exception"
+                print msg
+                s.close()
+                input.remove(s)
+
 
 server.close()
