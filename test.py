afd0d6a4dffdac9dd6a08e1eda32646166fbf044
Carl Schönbach <carl.schoenbach@gmail.com>
2014-04-28 17:46:27 +0200

    add a testsuite for urlmgr

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 from tests.stream import suite as streamSuite
 from tests.cache import suite as cacheSuite
 from tests.page import suite as pageSuite
+from tests.urlmgr import suite as urlmgrSuite
 import tools.log
 tools.log.dummy = 0 # for pylint
 
@@ -10,6 +11,7 @@ def test():
     runner.run(pageSuite())
     runner.run(streamSuite())
     runner.run(cacheSuite())
+    runner.run(urlmgrSuite())
 
 if __name__ == "__main__":
     test()
---
+++ tests/urlmgr.py
@@ -0,0 +1,28 @@
+import unittest
+import tools.log
+tools.log.dummy = 0 # for pylint
+import logging
+from tools.url import UrlMgr
+log = logging.getLogger()
+
+class UrlMgrTests(unittest.TestCase):
+    def CheckCookies(self):
+        # checking once for post and once for get
+        url = UrlMgr(url='http://httpbin.org/cookies/set/sessioncookie/123456789', nocache=True)
+        cookieData = url.data
+        self.assertGreater(cookieData.find("123456789"), 1, "the cookie wasn't correctly set")
+        url = UrlMgr(url="http://httpbin.org/cookies", nocache=True)
+        self.assertEquals(cookieData, url.data)
+        url = UrlMgr(url="http://httpbin.org/cookies", postdata='ads=asd', nocache=True)
+        self.assertEquals(cookieData, url.data)
+
+def suite():
+    urlmgr_suite = unittest.makeSuite(UrlMgrTests, "Check")
+    return unittest.TestSuite((urlmgr_suite))
+
+def test():
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+if __name__ == "__main__":
+    test()
