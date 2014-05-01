0b4a26822b78b0dbbc26dce8b33e309186e32adf
Carl Schönbach <carl.schoenbach@gmail.com>
2014-05-01 14:27:15 +0200

    simplified prog.py through putting code in more methods

--- prog.py
+++ prog.py
@@ -15,60 +15,66 @@ log = logging.getLogger(__name__)
 
 def main():
     link = config.link
-    media = None
     streamHandler = None
 
     if not link:
         import tools.commandline as com
         com.usage()
 
+    downloader = Downloader()
+
+    # a link can be either a download-page or a stream
     pageHandler = pages.getExtensionByRegexStringMatch(link)
     if not pageHandler:
         streamHandler = flashExt.getExtensionByRegexStringMatch(link)
         if not streamHandler:
             log.error('No handler for %s', link)
             sys.exit(1)
+        processStream(streamHandler, link, downloader)
     else:
-        pageHandler = pageHandler()
-        log.info("use pagehandler: %s", pageHandler.name)
-        media = pageHandler.get(link) # returns array of medias (extractAll) or just one media (download)
-        if is_array(media):
-            allPages = media
-            from tools.db2 import persist
-            persist(pageHandler, allPages)
-            log.info("finished")
-            sys.exit(0)
-        if not media:
-            log.error('Could not extract')
-            return
-
-    downloader = Downloader()
-
-    if media:
-        for part in media.getSubs():
-            queueData = []
-            for alt in part.getSubs():
-                altPartsPinfo = []
-                for altPart in alt.getSubs():
-                    pinfo = altPart.pinfo
-                    if not pinfo or not pinfo.title or not pinfo.stream_url:
-                        # this must be called before flv_url, else it won't work (a fix for this would cost more performance and more code)
-                        continue
-                    log.info('added "%s" to downloadqueue with "%s"', pinfo.title, pinfo.url)
-                    altPartsPinfo.append(pinfo)
-                if altPartsPinfo != []:
-                    queueData.append(altPartsPinfo)
-            downloader.download_queue.append(queueData)
-    elif streamHandler:
-        name = "tmp"
-        title = "tmp"
-        if config.dl_name:
-            name = config.dl_name
-        if config.dl_title:
-            title = config.dl_title
-        pinfo = VideoInfo(link)
-        pinfo.name = name
-        pinfo.title = title
-        downloader.download_queue.append([pinfo])
-
+        processPage(pageHandler, link, downloader)
+    # now the downloading starts
     downloader.run()
+
+
+def processMultiPage(pageHandler, media):
+    from tools.db2 import persist
+    persist(pageHandler, media)
+    log.info("finished")
+    sys.exit(0)
+
+def processPage(pageHandler, link, downloader):
+    pageHandler = pageHandler()
+    log.info("use pagehandler: %s", pageHandler.name)
+    media = pageHandler.get(link) # returns array of medias (extractAll) or just one media (download)
+    if not media:
+        log.error('Could not extract')
+        return False
+    if is_array(media):
+        return processMultiPage(pageHandler, media)
+    for part in media.getSubs():
+        queueData = []
+        for alt in part.getSubs():
+            altPartsPinfo = []
+            for altPart in alt.getSubs():
+                pinfo = altPart.pinfo
+                if not pinfo or not pinfo.title or not pinfo.stream_url:
+                    # this must be called before flv_url, else it won't work (a fix for this would cost more performance and more code)
+                    continue
+                log.info('added "%s" to downloadqueue with "%s"', pinfo.title, pinfo.url)
+                altPartsPinfo.append(pinfo)
+            if altPartsPinfo != []:
+                queueData.append(altPartsPinfo)
+        downloader.download_queue.append(queueData)
+
+def processStream(streamHandler, link, downloader):
+    name = "tmp"
+    title = "tmp"
+    if config.dl_name:
+        name = config.dl_name
+    if config.dl_title:
+        title = config.dl_title
+    pinfo = VideoInfo(link)
+    pinfo.name = name
+    pinfo.title = title
+    downloader.download_queue.append([pinfo])
