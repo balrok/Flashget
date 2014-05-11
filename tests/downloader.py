1e0c5bf7d1e64cc4db236f4ae84b30c10415cf64
Carl Schönbach <carl.schoenbach@gmail.com>
2014-05-11 23:19:48 +0200

    improved the downloader test
    still some todos but basically working

--- tests/downloader.py
+++ tests/downloader.py
@@ -12,19 +12,35 @@ tools.log.dummy = 0 # for pylint
 from tools.downloader import Downloader
 from tools.stream import VideoInfo
 log = logging.getLogger()
+from tools.url import LargeDownload
+LargeDownload.default_base_cache_dir = tempfile.mkdtemp()
 
 
 class DownloaderTests(unittest.TestCase):
     def CheckDownloader(self):
         downloader = Downloader(1)
+
         pinfo = VideoInfo('http://www.movshare.net/video/af6huuwg14nqo')
         pinfo.name = "testname"
         pinfo.title = "testtiel"
+
         cacheFolder = tempfile.mkdtemp()
         downloadPath = os.path.join(cacheFolder, pinfo.subdir, pinfo.title + ".flv")
+        print pinfo.stream
         pinfo.stream.get(pinfo) # call this, so flvUrl is set inside stream
         downloader.download_queue.append([[{'downloadPath': downloadPath, 'stream': pinfo.stream}]])
-        downloader.run()
+        # normally downloader.run() must be called - but I don't want to run it endlessly here
+        # so call this method and shortly after stop the downloading
+        downloader.downloadQueueProcessing()
+        self.assertEqual(len(downloader.current_downloads), 1)
+        # TODO dynamic sleeping
+        # TODO make a teardown method - so the assert here won't stop the call to end()
+        import time
+        time.sleep(3)
+        def testDownloads(dl):
+            self.assertGreaterEqual(dl['url'].downloaded, 5)
+            dl['url'].end()
+        downloader.iterateCurrentDownloads(testDownloads)
         # TODO - do some checks
         self.assertTrue(True)
 
