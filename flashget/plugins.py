b0b8b4bd48717c0b63f866de9f5ccdcf24fcfb06
Carl Mai <carl.schoenbach@gmail.com>
2015-10-09 20:14:50 +0200

    add videoinfo and plugins file

--- flashget/plugins.py
+++ flashget/plugins.py
@@ -1,2 +1,75 @@
+from .page import Page
+from .stream import BaseStream
+from .config import config
+
+try:
+    from compatible.urlresolver.curlresolver import getPlugins as getURPlugins
+    from compatible.urlresolver.streamhandler_urlresolver import Urlresolver
+except:
+    ur_plugins = False
+else:
+    ur_plugins = True
+
+from yapsy.PluginManager import PluginManager
+from yapsy.PluginFileLocator import PluginFileLocator, PluginFileAnalyzerMathingRegex
+
+import re
+import os
+
+plugins = PluginManager(plugin_locator=PluginFileLocator([PluginFileAnalyzerMathingRegex("all", "^[a-zA-Z][a-zA-Z_]*\.py$")]))
+plugins.setCategoriesFilter({
+   "Page" : Page,
+   "Stream" : BaseStream,
+})
+
+def loadExtension():
+    # folder from this project
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    pathes = []
+    pathes.append(os.path.join(path, 'pages'))
+    pathes.append(os.path.join(path, 'streams'))
+    # folder from config
+    for con in ('page_extension_dir', 'stream_extension_dir'):
+        c_path = config.get(con, "")
+        if len(c_path) > 1:
+            pathes.append(c_path)
+    plugins.setPluginPlaces(pathes)
+    plugins.collectPlugins()
+
 def getPageByLink(link):
-    pass
+    loadExtension()
+    for pluginInfo in plugins.getPluginsOfCategory("Page"):
+        if re.match(pluginInfo.plugin_object.eregex, link):
+            pluginInfo.plugin_object.setLink(link)
+            return pluginInfo.plugin_object
+def getAllPages():
+    loadExtension()
+    return [(p.plugin_object, p.path) for p in plugins.getPluginsOfCategory("Page")]
+
+def getStreamByLink(link):
+    loadExtension()
+    for pluginInfo in plugins.getPluginsOfCategory("Stream"):
+        print pluginInfo.plugin_object.eregex
+        if re.match(pluginInfo.plugin_object.eregex, link):
+            pluginInfo.plugin_object.setLink(link)
+            return pluginInfo.plugin_object
+    if ur_plugins:
+        for stream in getURPlugins():
+            for d in stream.domains:
+                if d != "*" and re.match(".*%s.*"%d, link):
+                    plugin = Urlresolver()
+                    plugin.setLink(link)
+                    return plugin
+
+def getAllStreams():
+    loadExtension()
+    streams = [(p.plugin_object, p.path) for p in plugins.getPluginsOfCategory("Stream")]
+    if ur_plugins:
+        for stream in getURPlugins():
+            stream.ename = stream.name
+            stream.url = "::".join(stream.domains)
+            streams.append((stream, stream.__class__.__name__))
+    return streams
+
+
--- flashget/videoinfo.py
+++ flashget/videoinfo.py
@@ -1,2 +1,98 @@
+
+import logging
+
+from .url import UrlMgr
+from .helper import urldecode, normalize_title
+
+log = logging.getLogger(__name__)
+
+# maintains lowlevel information about the video file
+# basically name, title and stream object
 class VideoInfo(object):
-    pass
+    def __init__(self, url):
+        self.subdir = ""
+        self.flv_url = ""
+        self.title = ""
+        self.has_stream = False
+        # don't set the following values because they are set by getattr
+        # self.stream_id = ""
+        # self.stream = None
+        if isinstance(url, UrlMgr):
+            self.stream_url = url.url
+        else:
+            self.stream_url = urldecode(url)
+
+    def __hash__(self):
+        # the hash will always start with "h" to create also a good filename
+        # hash will be used, if title-extraction won't work
+        return 'h %s' % hash(self.stream_url)
+
+    def __getattr__(self, key):
+        if key == 'subdir':
+            return self.get_subdir()
+        elif key == 'stream_url':
+            return self.get_stream()
+        elif key == 'stream':
+            self.get_stream()
+            return self.stream
+        elif key == 'stream_id':
+            self.get_stream()
+            return self.stream_id
+        elif key == 'flv_url':
+            return self.get_flv()
+        elif key == 'flv_type':
+            if self.has_stream:
+                self.flv_type = self.stream.ename
+            else:
+                self.flv_type = None
+            return self.flv_type
+
+    def __repr__(self):
+        return "%s: %s .-. %s" % (self.__class__.__name__, self.flv_type, self.title)
+
+    def get_subdir(self):
+        self.subdir = self.name
+        return self.subdir
+
+    def get_flv(self):
+        self.flv_url = self.stream.get(self)
+        return self.flv_url
+
+    def get_title(self):
+        log.error("TITLE must be downloaded from overviewpage")
+        if not self.title:
+            # it isn't fatal if we don't have the title, just use the own hash, which should be unique
+            # maybe in future, we should set a variable here, so that we know from outside,
+            # if the title is only the hash and we need to discover a better one
+            self.title = hash(self)  # normalize_title isn't needed, the hash will make sure that the title looks ok
+            log.info('couldnt extract title - will now use the hash from this url: %s', self.title)
+        else:
+            self.title = normalize_title(self.title)
+        return self.title
+
+    def get_stream(self):
+        # to avoid recursive dependencies, import it here
+        from .plugins import getStreamByLink
+
+        stream = getStreamByLink(self.stream_url)
+        if self.flv_type: # normally a stream knows its flv_type - but redirection pages don't..
+            stream.flv_type = self.flv_type
+
+        # this would open the page and look for common flash embedding to find a link for the download
+        # I think this code doesn't belong here and should go to each individual page extractor (only if needed - most won't need this)
+        # if stream is None:
+        #     streamData = extract_stream(UrlMgr(url=self.stream_url).data)
+        #     if streamData and streamData['url']:
+        #         stream = findStream(streamData['url'])
+        #         self.stream_url = streamData['url']
+
+        if stream is None:
+            log.warning('couldn\'t find a supported streamlink in: %s', self.stream_url)
+            self.stream_url = None
+            self.stream = None
+            self.stream_id = None
+            return None
+        self.stream = stream
+        self.stream_id = stream.getId()
+        self.has_stream = True
+        return self.stream_url
