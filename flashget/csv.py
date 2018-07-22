a63012e85d55dc0fb584be62e16bc38003268dcf
Carl Mai <carl.schoenbach@gmail.com>
2018-07-22 22:26:19 +0200

    enhanced the csv library a bit

--- flashget/csv.py
+++ flashget/csv.py
@@ -1,15 +1,30 @@
 import io
-
-file_handle = None
+import csv as csvlib
 
 class csv(object):
-    def create():
+    file_handle = None
+    def create(name = "flashget.csv"):
         global file_handle
-        file_handle = io.open("flashget.csv", 'w', encoding="utf-8")
+        file_handle = io.open(name, 'w', encoding="utf-8")
 
     def append(downloadPath, url):
         global file_handle
-        file_handle.write('"%s", "%s"\n' % (downloadPath.replace('"', '\\"'), url.replace('"', '\\"')))
+        spamwriter = csvlib.writer(file_handle, delimiter=',', quotechar='"', quoting=csvlib.QUOTE_ALL)
+        spamwriter.writerow([downloadPath, url])
+        # file_handle.write('"%s","%s"\n' % (downloadPath.replace('"', '\\"'), url.replace('"', '\\"')))
+
+    def read(name = "flashget.csv"):
+        with io.open(name, encoding="utf-8") as f:
+            data = csvlib.reader(f, delimiter=',', quotechar='"')
+            return list(data)
+
+    def read_combined(name = "flashget.csv"):
+        data = csv.read(name)
+        combined = {}
+        for line in data:
+            entry = combined.get(line[0], [])
+            combined[line[0]] = entry + [line[1]]
+        return combined
 
     def close():
         global file_handle
